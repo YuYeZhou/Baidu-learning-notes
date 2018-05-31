086990d481a59fee8503968d3670998e57a307e0
Yezhou <34479092+YuYeZhou@users.noreply.github.com>
2018-05-31 19:48:10 +0800

    Add files via upload

---
+++ 百度前端学院第12天第15天/js/comWeb.js
@@ -0,0 +1,12 @@
+var  scrollHeight = document.documentElement.offsetWidth;
+
+// var html = document.getElementsByTagName('html');
+// html.style.height = scrollHeight+'px'
+
+var body = document.getElementsByTagName('body');
+body.style.height = scrollHeight+'px'
+
+console.log(scrollHeight);
+
+
+
